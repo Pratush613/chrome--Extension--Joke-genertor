0c90957715d3c0873f524932f1be2729f8b106c1
Pratush613 <pratushsinha619@gmail.com>
2024-02-17 19:37:54 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+setInterval(()=>{  
+   fetch("https://icanhazdadjoke.com/slack")
+  .then( data => data.json())
+   .then(jokedata =>{
+      const Joke = jokedata.attachments[0].text;
+      const jokeElement = document.getElementById("jokeElements");
+      jokeElement.innerHTML = Joke;
+   })
+},2000)
